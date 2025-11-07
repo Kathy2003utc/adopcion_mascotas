16e4e67f31bd72f1a7aa80658b6b78646bc6105a
NASHELY MIKAELA BETANCOURT TOSCANO <nashely.betancourt6156@utc.edu.ec>
2025-11-06 22:09:08 -0500

    agregar validaciones en editar usuarios

--- Adopcion_mascotas/settings.py
+++ Adopcion_mascotas/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'adopcion_db',
         'USER': 'postgres',
-        'PASSWORD': 'K@thy2003',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '5432',
     }
